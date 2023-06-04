ba02cbaa283a1754315237e34423e17631ae2df3
girma <girmatarekegn.gi@gmail.com>
2023-06-04 13:10:56 +0300

    Add spread string and array form

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  const array3 = Array.from(string);
-
-  return [...array1, ...array2, ...array3];
+  return [...array1, ...array2, ...string];
 }
